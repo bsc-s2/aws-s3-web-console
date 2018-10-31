0cf2125bd6fcbb68d7fbc9f8c4a1ad3febfd5a52
lan <adwerrd@hotmail.com>
2018-10-31 17:13:56 +0800

    modify post method

--- src/service/aws-http.js
+++ src/service/aws-http.js
@@ -1,19 +1,42 @@
+import { Message } from 'element-ui'
 import store from '@/store'
 
 let host = 'http://47.75.196.0:8080/'
 
-function post(url, params) {
-  return fetch(host + url, {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: store.state.token || '',
-    },
-    method: 'post',
-    body: JSON.stringify(params),
-  }).then((res) => {
-    return res.json()
+function parseJson(response) {
+  return response.json().then((data) => {
+    return {
+      status: response.status,
+      data,
+    }
   })
+}
+
+function post(url, params) {
+  try {
+    return new Promise((resolve, reject) => {
+      fetch(host + url, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: store.state.token || '',
+        },
+        method: 'post',
+        body: JSON.stringify(params),
+      })
+        .then(parseJson)
+        .then(({ status, data }) => {
+          if (status === 200) {
+            return resolve(data)
+          } else {
+            Message.error({ message: `Error: ${status} - ${data.message}` })
+            return reject(status, data)
+          }
+        })
+    })
+  } catch (e) {
+    return Promise.reject(e)
+  }
 }
 
 function login(params) {
